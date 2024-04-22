7d379b4949798d8375aaef58861b743fb59c437a
Sudhir Kumar <sudhirkumarjnv2k12@gmail.com>
2024-04-22 18:59:35 +0530

    ...

--- PrintingPatterns/HalfPyramid.cpp
+++ PrintingPatterns/HalfPyramid.cpp
@@ -9,8 +9,12 @@
 using namespace std;
 int main(){
 
-    int r=5, c=5;
-    int i=1;
+    int r, c;
+    
+    cout << "Rows : " ;
+    cin >>  r; //5
+    cout << "Column : " ;
+    cin >>  c; //5
 
     for(int i=1; i<=r; i++){
         for(int j=1; j<=c; j++){
