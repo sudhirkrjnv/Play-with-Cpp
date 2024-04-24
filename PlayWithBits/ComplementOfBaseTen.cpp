5d317737717eaf59b6cbbe85350192ae0c6fee63
Sudhir Kumar <sudhirkumarjnv2k12@gmail.com>
2024-04-24 20:14:46 +0530

    ...

---
+++ PlayWithBits/ComplementOfBaseTen.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+    int n = 5;
+    int m = n;
+
+    if (n == 0){
+        cout << 1;
+    }
+
+    int mask  = 0;
+
+    while( m !=0 ){
+        mask = (mask << 1) + 1; 
+        m = m >> 1;
+    }
+    int ans = (~n) & mask;
+
+    cout << ans << endl;
+    cout << (~5);
+
+}
