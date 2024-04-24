8fb241b61023f835ce5f4f24d7b303cf15834eda
Sudhir Kumar <sudhirkumarjnv2k12@gmail.com>
2024-04-24 09:04:40 +0530

    ...

--- PlayWithBits/DecimalToBinary.cpp
+++ PlayWithBits/DecimalToBinary.cpp
@@ -10,12 +10,12 @@ int main() {
     deque<int> d;
 
     while (decimal > 0) {
-        int r = decimal % 2;
-        d.push_front(r);
+        int rem = decimal % 2;
+        d.push_front(rem);
         decimal = decimal / 2;
     }
 
-    cout << "Binary equivalent: ";
+    cout << "Binary Code : ";
     for(int i: d){
         cout << i ;
     }
