7172de850bd95f5da9cb51aea30eef39d493b62b
Sudhir Kumar <sudhirkumarjnv2k12@gmail.com>
2024-05-05 08:43:27 +0530

    TCS 26 Apr, 2024

--- Recursion/FindTotalPathInMatrix.cpp
+++ Recursion/FindTotalPathInMatrix.cpp
@@ -10,12 +10,12 @@
 #include<iostream>
 using namespace std;
 
-int findTotalPath(int *arr, int i, int j, int m, int n){
+int findTotalPath(int **arr, int i, int j, int m, int n){
     // Base case
     if (i == m-1 && j == n-1){
         return 1;
     }
-    if (i >= m || j >= n || arr[i*n+j] == 1){
+    if (i >= m || j >= n || arr[i][j] == 1){
         return 0;
     }
 
@@ -24,8 +24,19 @@ int findTotalPath(int *arr, int i, int j, int m, int n){
 
 int main(){
 
-    int m = 3, n = 3;
-    int arr[m][n];
+    int m, n;
+
+    cout << "Enter no of Rows : ";
+    cin >> m;
+    cout << "Enter no of Column : ";
+    cin >> n;
+    
+
+    //creating a 2-D array
+    int** arr = new int *[m];
+    for(int i= 0; i < n ; i++){
+        arr[i] = new int [n];
+    }
 
     cout << "Enter matrix (0 for empty cell, 1 for obstacle):" << endl;
 
@@ -36,9 +47,8 @@ int main(){
         }
     }
 
-    int i = 0;
-    int j = 0;
-    int ans = findTotalPath((int *)arr, i, j, m, n);
+    
+    int ans = findTotalPath(arr, 0, 0, m, n);
     cout << "Total number of paths: " << ans << endl;
 
     /*
