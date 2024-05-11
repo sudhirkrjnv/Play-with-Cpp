0344e3a2de7c21628806d985aad8b1f40ccec26b
Sudhir Kumar <sudhirkumarjnv2k12@gmail.com>
2024-05-11 10:44:44 +0530

    TCS May 08, 2024

--- Basic Maths/PrintingNumbers/PrimeWithDigitSumAlsoPrime.cpp
+++ Basic Maths/PrintingNumbers/PrimeWithDigitSumAlsoPrime.cpp
@@ -17,8 +17,8 @@ bool isPrime(int n){
 int digitSum(int n){
     int sum = 0;
     while (n > 0) {
-        sum += n % 10;
-        n /= 10;
+        sum = sum + (n % 10);
+        n = n / 10;
     }
     return sum;
 }
