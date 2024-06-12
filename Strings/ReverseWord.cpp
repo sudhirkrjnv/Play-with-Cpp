6eafb70ff588708e378bf1ca497428f986111e22
Sudhir Kumar <sudhirkumarjnv2k12@gmail.com>
2024-06-12 20:07:40 +0530

    ...

---
+++ Strings/ReverseWord.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+void ReverseWord(string str){
+    stack<string> NewWord;
+    NewWord = "";
+
+}
+int main(){
+    string str = " My Name is Sudhir Kumar";
+
+}
