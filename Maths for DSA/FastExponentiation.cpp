94b7d03f3bf260fff759ba5a9ec947bf1321629f
Sudhir Kumar <sudhirkumarjnv2k12@gmail.com>
2024-06-24 12:27:40 +0530

    .

---
+++ Maths for DSA/FastExponentiation.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+double power(double a, int n){
+    double ans = 1;
+    
+    if (n < 0) {
+        a = 1 / a;
+        n = -n;
+    }
+    while(n > 0){
+        if(n&1){
+            ans = ans * a;
+        }
+        a = a*a;
+        n = n >> 1;
+    }
+    return ans;
+}
+
+int main(){
+
+    // a^n = ans
+
+    double x;
+    cin >> x;  //input for base
+
+    int n;
+    cin >> n;  // input for power
+
+    double mypower = power(x, n);
+    cout << mypower;
+
+    return 0;
+}
